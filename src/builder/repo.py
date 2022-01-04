3df87c5766fba05f59705cfabf8fc790fb348047
david <davidventura27@gmail.com>
2022-01-04 22:28:37 +0100

    use checkout

--- src/builder/repo.py
+++ src/builder/repo.py
@@ -57,8 +57,7 @@ class Repo:
         shutil.rmtree(path, ignore_errors=True)
         path.mkdir(parents=True, exist_ok=True)
         r = git.Repo.clone_from(self.repo_url, path)
-        r.remotes.origin.fetch()
-        r.head.reference = ref
+        r.checkout(ref)
         self.local_path = path
         return path
 
